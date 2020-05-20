1353cc0b7199bc07c0a1b632ea926ee729d5aa39
Caroline Brownlee <caroline.brownlee@carecorenational.com>
2020-05-20 12:53:42 -0500

    getting error in controllers.js module

---
+++ Beginner/Controllers/controllers.js
@@ -0,0 +1,9 @@
+var myApp = angular.module('myApp');
+
+myApp.controller('mainctrl',['$scope', function($scope){
+   $scope.message='Hello Angular!';
+}]);
+// var MainCtrl = function($scope){
+//     $scope.message = "Hello, Angular!";
+// };
+
