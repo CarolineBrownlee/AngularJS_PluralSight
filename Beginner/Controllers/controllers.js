c544499e3c0e40b3988d631b813393cd2de28ef6
Caroline Brownlee <caroline.brownlee@carecorenational.com>
2020-05-20 13:24:31 -0500

    stupid error

--- Beginner/Controllers/controllers.js
+++ Beginner/Controllers/controllers.js
@@ -1,9 +1,4 @@
-var myApp = angular.module('myApp');
-
-myApp.controller('mainctrl',['$scope', function($scope){
-   $scope.message='Hello Angular!';
-}]);
-// var MainCtrl = function($scope){
-//     $scope.message = "Hello, Angular!";
-// };
+var MainCtrl = function($scope){
+    $scope.message = "Hello, Angular!";
+};
 
