4b342e6a89f8120bb29563724c354f1a51817082
Caroline Brownlee <caroline.brownlee@carecorenational.com>
2020-05-21 11:56:37 -0500

    data binding, created a person object and attached it to scope, everything works correctly, commented code for reference

--- Beginner/Controllers/controllers.js
+++ Beginner/Controllers/controllers.js
@@ -1,7 +1,16 @@
 var myApp = angular.module('myApp', []);
 
+// created a person object
+var person = {
+    firstName: "Caroline",
+    lastName: "Brownlee",
+    imageSrc: "https://carolinemadisondotcom.files.wordpress.com/2020/04/caroline-and-joseph.jpg?w=434&h=434"
+};
+
 myApp.controller('myCtrl', function($scope){
     $scope.message='Hello Angular!';
+    // attaching person object to scope
+    $scope.person = person;
 });
 
 
