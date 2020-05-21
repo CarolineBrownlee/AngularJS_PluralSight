8fca6ecf7b59f5ca0c2b5199b225bfde6c6c1fe3
Caroline Brownlee <caroline.brownlee@carecorenational.com>
2020-05-21 14:58:40 -0500

    added example of http request

--- Beginner/Controllers/controllers.js
+++ Beginner/Controllers/controllers.js
@@ -13,6 +13,17 @@ myApp.controller('myCtrl', function($scope){
     $scope.person = person;
 });
 
+// $http Service
+// Encapsulates HTTP communication:  GET, POST, PUT, DELETE
+// Can "ask" for $http inside a controller by passing as a parameter
+// Sample Code:
+var PersonController = function($scope, $http) {
 
-
-          
+    var promise = $http.get("/users/1783")
+    promise.then(function(response) {
+        $scope.user = response.data;
+    });
+    // THIS CODE RETURNS A PROMISE: a promise to deliver a value in the future
+    // refactored code above stores returned data into a variable and then puts the data into scope
+    // $scope.user = $http.get("/users/1783");
+}
