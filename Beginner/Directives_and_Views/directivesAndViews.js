183e3d4dbcfb8c7f1cd40af864d78aaf929dbcd3
Caroline Brownlee <caroline.brownlee@carecorenational.com>
2020-05-23 08:09:26 -0500

    added some styling and controller for search

--- Beginner/Directives_and_Views/directivesAndViews.js
+++ Beginner/Directives_and_Views/directivesAndViews.js
@@ -10,6 +10,12 @@ gitHubApp.controller("gitHubCtrl", function($scope, $http) {
         $scope.error = "Sorry, I can't get that information for you at the moment."
     }
 
-    $http.get("http://api.github.com/users/CarolineBrownlee")
-        .then(gitHubResponse, errorResponse);
+    // Create a search attribute on $scope and have that equal to a function that takes a username parameter and then send off the request
+    $scope.search = function(username) {
+        $http.get("http://api.github.com/users/" + username)
+            .then(gitHubResponse, errorResponse)
+    };
+
+        $scope.message = "GitHub Viewer Application";
+        $scope.username = "";
 });
