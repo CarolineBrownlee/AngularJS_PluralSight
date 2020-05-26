42ed2a75aa3eae226df882242c8cf9496e8dc626
Caroline Brownlee <brownleecaroline@gmail.com>
2020-05-26 08:59:03 -0500

    repos show in table

--- Beginner/Directives_and_Views/directivesAndViews.js
+++ Beginner/Directives_and_Views/directivesAndViews.js
@@ -4,7 +4,14 @@ gitHubApp.controller("gitHubCtrl", function($scope, $http) {
 
     var gitHubResponse = function(response){
         $scope.user = response.data;
+        $http.get($scope.user.repos_url)
+            .then(onRepos, errorResponse)  
+        console.log($scope)
     };
+    
+    var onRepos = function(response) {
+        $scope.repos = response.data;
+    }
 
     var errorResponse = function(reason) {
         $scope.error = "Sorry, I can't get that information for you at the moment."
@@ -13,7 +20,8 @@ gitHubApp.controller("gitHubCtrl", function($scope, $http) {
     // Create a search attribute on $scope and have that equal to a function that takes a username parameter and then send off the request
     $scope.search = function(username) {
         $http.get("http://api.github.com/users/" + username)
-            .then(gitHubResponse, errorResponse)
+        .then(gitHubResponse, onRepos, errorResponse)
+        console.log(username)
     };
 
         $scope.message = "GitHub Viewer Application";
